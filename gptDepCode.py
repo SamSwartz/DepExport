d8b39081d309dc021fdb9809ddc0ea57899891e1
SamSwartz <swart124@d.umn.edu>
2023-08-02 15:32:34 -0500

    Working, but ID's are not correct

--- gptDepCode.py
+++ gptDepCode.py
@@ -1,6 +1,8 @@
 import requests
 import webbrowser
 import openpyxl
+import os
+from datetime import datetime
 
 # OAuth2 credentials - Replace with your actual credentials
 CLIENT_ID = '4e97513dc1aee76830dd43ba617eaccf97f87bef'
@@ -46,7 +48,7 @@ def get_access_token():
         print("Failed to get access token.")
         return None
     
-def get_all_users():
+def get_all_users(access_token):
     url = f"{BASE_URL}/resource/Employee/QUERY"
     headers = {"Authorization": f"Bearer {access_token}"}
 
@@ -63,16 +65,32 @@ def get_all_users():
         print(f"Failed to get users. Error: {response.status_code}")
         return None
     
-#def get_timesheets_hours(access_token, user_id):
+def get_timesheets_hours(access_token, user_id):
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
-    url = f'{BASE_URL}/resource/TimesheetPayReturn/QUERY'
+    url = f'{BASE_URL}/resource/Timesheet/QUERY'
     params = {
-        'user': user_id,
+        'search': {
+            's1': {
+                'field': 'Employee',
+                'data': user_id,
+                'type': 'eq',
+            },
+            's2': {
+                'field': 'Date',
+                'data': '2022-10-01',
+                'type': 'gt',
+            },
+            's3': {
+                'field': 'Date',
+                'data': '2022-10-15',
+                'type': 'lt',
+            },
+        },
     }
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.post(url, headers=headers, json=params)
 
     if response.status_code == 200:
         return response.json()
@@ -80,7 +98,42 @@ def get_all_users():
         print(f"Failed to get timesheets for user {user_id}. Error: {response.status_code}")
         return None
 
-def write_to_excel(data):
+# Additional function to get timesheets for a specific date range
+def get_timesheets_by_date_range(access_token, user_id, start_date, end_date):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    url = f'{BASE_URL}/resource/Timesheet/QUERY'
+    params = {
+        'search': {
+            's1': {
+                'field': 'Employee',
+                'data': user_id,
+                'type': 'eq',
+            },
+            's2': {
+                'field': 'Date',
+                'data': start_date,
+                'type': 'gt',
+            },
+            's3': {
+                'field': 'Date',
+                'data': end_date,
+                'type': 'lt',
+            },
+        },
+    }
+
+    response = requests.post(url, headers=headers, json=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to get timesheets for user {user_id}. Error: {response.status_code}")
+        return None
+
+
+def write_to_excel(data, start_date, end_date):
     workbook = openpyxl.Workbook()
     sheet = workbook.active
     sheet.title = 'Timesheets Hours'
@@ -90,39 +143,48 @@ def write_to_excel(data):
     for idx, timesheet in enumerate(data):
         # Assuming the structure of the timesheet data
         # Replace these keys with actual keys from the API response
-        user_id = timesheet['user_id']
-        start_time = timesheet['start_time']
-        end_time = timesheet['end_time']
-        total_hours = timesheet['total_hours']
+        user_id = timesheet['Id']
+        start_time_unix = timesheet['StartTime']
+        end_time_unix = timesheet['EndTime']
+        total_time = '{:.2f}'.format(timesheet['TotalTime'])  # Convert to string with 2 decimal places
+
+        # Format the start and end times in a human-readable format
+        start_time = datetime.fromtimestamp(start_time_unix).strftime('%Y-%m-%d %I:%M:%S %p')
+        end_time = datetime.fromtimestamp(end_time_unix).strftime('%Y-%m-%d %I:%M:%S %p')
 
         # Write the data to the Excel worksheet
-        row = [idx + 1, user_id, start_time, end_time, total_hours]
+        row = [idx + 1, user_id, start_time, end_time, total_time]
         sheet.append(row)
 
-    workbook.save('timesheets_hours.xlsx')
-    print('Data written to Excel successfully.')
+    # Specify the full path where you want to save the file
+    full_path = os.path.join(os.getcwd(), 'timesheets_hours.xlsx')
+
+    workbook.save(full_path)
+    print(f'Data written to Excel successfully. File saved at: {full_path}')
 
 def main():
-    # Replace 'user_id' with the actual user ID for whom you want to retrieve timesheets
-    user_id = 'user_id'
-
-    # Step 1: Get the access token using OAuth2
+   # Step 1: Get the access token using OAuth2
     access_token = get_access_token()
 
     if access_token:
         # Step 2: Get all users
         users_data = get_all_users(access_token)
 
-    if users_data:
-        all_timesheets_data = []
-        for user in users_data:
-            user_id = user['Id']
-            timesheets_data = get_timesheets_hours(access_token, user_id)
-            if timesheets_data:
-                all_timesheets_data.extend(timesheets_data)
+        if users_data:
+            all_timesheets_data = []
 
-        # Step 3: Write all timesheets data to Excel
-        write_to_excel(all_timesheets_data)
+            # Get user input for date range
+            start_date = input("Enter the start date (YYYY-MM-DD): ")
+            end_date = input("Enter the end date (YYYY-MM-DD): ")
+
+            for user in users_data:
+                user_id = user['Id']
+                timesheets_data = get_timesheets_by_date_range(access_token, user_id, start_date, end_date)
+                if timesheets_data:
+                    all_timesheets_data.extend(timesheets_data)
+
+            # Step 3: Write all timesheets data to Excel
+            write_to_excel(all_timesheets_data, start_date, end_date)
 
 if __name__ == '__main__':
     main()
