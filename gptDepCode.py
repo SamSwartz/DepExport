f1e92e7e65cc46bd788721485e343a6692974c61
SamSwartz <swart124@d.umn.edu>
2023-08-09 16:30:21 -0500

    Working but Sleep Times show 0

--- gptDepCode.py
+++ gptDepCode.py
@@ -112,6 +112,39 @@ def get_employee_by_id(access_token, user_id):
         print(f"Failed to get employee {user_id}. Error: {response.status_code}")
         return None
 
+def get_timesheets_by_operational_unit(access_token, operational_unit_name, start_date, end_date):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    url = f'{BASE_URL}/resource/Timesheet/QUERY'
+    params = {
+        'search': {
+            's1': {
+                'field': 'OperationalUnitName',
+                'data': operational_unit_name,
+                'type': 'eq',
+            },
+            's2': {
+                'field': 'Date',
+                'data': start_date,
+                'type': 'gt',
+            },
+            's3': {
+                'field': 'Date',
+                'data': end_date,
+                'type': 'lt',
+            },
+        },
+        'fields': 'Employee,TotalTime',
+    }
+
+    response = requests.post(url, headers=headers, json=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to get timesheets for operational unit {operational_unit_name}. Error: {response.status_code}")
+        return None
     
 # Additional function to get timesheets for a specific date range
 def get_timesheets_by_date_range(access_token, user_id, start_date, end_date):
@@ -142,7 +175,7 @@ def get_timesheets_by_date_range(access_token, user_id, start_date, end_date):
 
     response = requests.post(url, headers=headers, json=params)
 
-    print("Response Content:", response.text)
+   # print("Response Content:", response.text)
     if response.status_code == 200:
         return response.json()
     else:
@@ -153,54 +186,101 @@ def get_employee_name_and_area(access_token, user_id):
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
-    url = f'{BASE_URL}/resource/Employee/{user_id}'
+    url = f'{BASE_URL}/resource/OperationalUnit/INFO'
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        display_name = data.get('DisplayName', '')
-        area_data = data.get('Areas', [])  # 'Areas' may contain a list of area data
+        fields = data.get('fields', {})
 
-        # Assuming 'Areas' contains a list, we can choose the first item to get the area name
-        area_name = area_data[0]['AreaName'] if area_data else ''
-        return display_name, area_name
+        if 'OperationalUnitName' in fields:
+            employee_url = f'{BASE_URL}/resource/Employee/{user_id}'
+            employee_response = requests.get(employee_url, headers=headers)
+
+            if employee_response.status_code == 200:
+                employee_data = employee_response.json()
+                display_name = employee_data.get('DisplayName', '')
+                area_data = employee_data.get('Areas', [])
+
+                area_name = ''
+
+                if isinstance(area_data, list) and area_data:
+                    for area_item in area_data:
+                        area_name += area_item.get('OperationalUnitName', '') + ', '
+
+                    # Remove trailing comma and space
+                    area_name = area_name.rstrip(', ')
+
+                return display_name, area_name
+            else:
+                print(f"Failed to get employee {user_id}. Error: {employee_response.status_code}")
+        else:
+            print("OperationalUnitName is not available in the API response.")
     else:
-        print(f"Failed to get employee {user_id}. Error: {response.status_code}")
-        return '', ''
+        print(f"Failed to get OperationalUnit INFO. Error: {response.status_code}")
+
+    return '', ''
 
 
-def write_to_excel(data, access_token, start_date, end_date):
+def write_to_excel(sleep_timesheets_data, other_timesheets_data, access_token, start_date, end_date):
     workbook = openpyxl.Workbook()
     sheet = workbook.active
     sheet.title = 'Timesheets Hours'
 
     # Write the header row
-    header = ['Index', 'User ID', 'Display Name', 'Area', 'Start Time', 'End Time', 'TotalTime']
+    header = ['Index', 'User ID', 'Display Name', 'Total Sleep Time', 'Total Other Time']
     sheet.append(header)
 
-    for idx, timesheet in enumerate(data):
+    # Create dictionaries to store sleep and other time totals per employee
+    sleep_totals = {}
+    other_totals = {}
+
+    # Process sleep timesheets data
+    for idx, timesheet in enumerate(sleep_timesheets_data):
         user_id = timesheet.get('Employee', '')
         display_name, area = get_employee_name_and_area(access_token, user_id)
+        total_time = timesheet['TotalTime']
+        
+ # Categorize timesheets as sleep or other based on area
+        if area == 'Sleep':
+            sleep_totals[user_id] = sleep_totals.get(user_id, 0) + total_time
+        else:
+            other_totals[user_id] = other_totals.get(user_id, 0) + total_time
 
-        start_time_unix = timesheet['StartTime']
-        end_time_unix = timesheet['EndTime']
-        total_time = '{:.2f}'.format(timesheet['TotalTime'])  # Convert to string with 2 decimal places
+    # Process other timesheets data
+    for idx, timesheet in enumerate(other_timesheets_data):
+        user_id = timesheet.get('Employee', '')
+        display_name, area = get_employee_name_and_area(access_token, user_id)
+        total_time = timesheet['TotalTime']
 
-        # Format the start and end times in a human-readable format
-        start_time = datetime.fromtimestamp(start_time_unix).strftime('%Y-%m-%d %I:%M:%S %p')
-        end_time = datetime.fromtimestamp(end_time_unix).strftime('%Y-%m-%d %I:%M:%S %p')
+        # Categorize timesheets as sleep or other based on area
+        if area != 'Sleep':
+            other_totals[user_id] = other_totals.get(user_id, 0) + total_time
+
+    # Write sleep time totals for each employee
+    for idx, user_id in enumerate(sleep_totals):
+        display_name, _ = get_employee_name_and_area(access_token, user_id)
+        sleep_time = sleep_totals.get(user_id, 0)
 
         # Write the data to the Excel worksheet
-        row = [idx + 1, user_id, display_name, area, start_time, end_time, total_time]
+        row = [idx + 1, user_id, display_name, sleep_time, 0]  # Other time is 0 for sleep timesheets
         sheet.append(row)
 
-    workbook.save('timesheets_hours.xlsx')
+    # Write other time totals for each employee
+    for idx, user_id in enumerate(other_totals):
+        display_name, _ = get_employee_name_and_area(access_token, user_id)
+        other_time = other_totals.get(user_id, 0)
+
+        # Write the data to the Excel worksheet
+        row = [idx + 1, user_id, display_name, 0, other_time]  # Sleep time is 0 for other timesheets
+        sheet.append(row)
+
+    workbook.save('timesheets_totals.xlsx')
     print('Data written to Excel successfully.')
 
-
 def main():
-   # Step 1: Get the access token using OAuth2
+    # Step 1: Get the access token using OAuth2
     access_token = get_access_token()
 
     if access_token:
@@ -208,7 +288,8 @@ def main():
         users_data = get_all_users(access_token)
 
         if users_data:
-            all_timesheets_data = []
+            sleep_timesheets_data = []
+            other_timesheets_data = []
 
             # Get user input for date range
             start_date = input("Enter the start date (YYYY-MM-DD): ")
@@ -218,16 +299,18 @@ def main():
                 user_id = user['Id']
                 timesheets_data = get_timesheets_by_date_range(access_token, user_id, start_date, end_date)
                 if timesheets_data:
-                    all_timesheets_data.extend(timesheets_data)
-            
-            # if user_id is not None:
-            #     timesheets_data = get_timesheets_by_date_range(access_token, user_id, start_date, end_date)
-            #     if timesheets_data:
-            #         all_timesheets_data.extend(timesheets_data)
-            #Print Timesheets data
-            print(timesheets_data)
-            # Step 3: Write all timesheets data to Excel
-            write_to_excel(all_timesheets_data, access_token, start_date, end_date)
+                    user_operational_unit = user.get('OperationalUnitName', '')  # Replace with the actual field name
+                    total_time = sum(float(ts['TotalTime']) for ts in timesheets_data)
+
+                    if user_operational_unit == 'Sleep':
+                        sleep_timesheets_data.append({'Employee': user_id, 'TotalTime': total_time})
+                    else:
+                        other_timesheets_data.append({'Employee': user_id, 'TotalTime': total_time})
+            print("Sleep Timesheets Data:", sleep_timesheets_data)
+            print("Other Timesheets Data:", other_timesheets_data)
+            # Step 3: Write sleep and other timesheets data to Excel
+            write_to_excel(sleep_timesheets_data, other_timesheets_data, access_token, start_date, end_date)
 
 if __name__ == '__main__':
     main()
+
